1c6569802c7edf92de44fb6c2d68455e0403ef62
Mayank nautiyal <nautiyalmamta198@gmail.com>
2025-05-18 15:22:06 +0530

    Create Hyperledger_setup.bash

---
+++ Practice/Hyperledger fabric/Hyperledger_setup.bash
@@ -0,0 +1,13 @@
+# HYPERLEDGER - FABRIC
+1.  sudo apt install golang-go
+2.  docker –version
+3.  docker-compose –version
+4.  ls
+5.  git clone -b main https://github.com/hyperledger/fabric-samples.git
+6.  cd fabric-samples
+7.  curl -sSL https://bit.ly/2ysbOFE | bash -s
+8.  cd test-network
+9.   ./network.sh
+10.   ./network.sh up
+11.    ./network.sh createChannel
+12.    ./network.sh down
